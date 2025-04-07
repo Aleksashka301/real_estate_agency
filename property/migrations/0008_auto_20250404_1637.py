95555be2f33e39487f1408759c2dc413ead0ea17
Aleksashka301 <aleksashka301@gmail.com>
2025-04-07 19:18:03 +0500

    Update 0008_auto_20250404_1637.py

--- property/migrations/0008_auto_20250404_1637.py
+++ property/migrations/0008_auto_20250404_1637.py
@@ -6,12 +6,15 @@ import phonenumbers
 
 def normalize_phone_numbers(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
+    for flat in Flat.objects.iterator():
         if flat.owners_phonenumber:
             try:
                 parsed_number = phonenumbers.parse(flat.owners_phonenumber, 'RU')
                 if phonenumbers.is_valid_number(parsed_number):
-                    flat.owner_pure_phone = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
+                    flat.owner_pure_phone = phonenumbers.format_number(
+                        parsed_number,
+                        phonenumbers.PhoneNumberFormat.E164
+                    )
                     flat.save(update_fields=['owner_pure_phone'])
             except phonenumbers.NumberParseException:
                 pass
